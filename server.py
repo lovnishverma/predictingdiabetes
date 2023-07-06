8adda3805743a603a15d26b695a9dc3daae7efb6
Glitch (glitch-hello-website) <none>
2023-07-06 21:06:08 +0000

    🤑👐 Checkpoint ./server.py:57629255/7231 ./te.csv:57629255/33 ./requirements.txt:57629255/6 ./templates/index.html:57629255/4307

--- server.py
+++ server.py
@@ -6,10 +6,9 @@ from sklearn.linear_model import LogisticRegression
 app = Flask(__name__)
 
 def validate_input(input_data):
-    # Check if all input fields are provided
     required_fields = ["age", "hypertension", "heart_disease", "bmi", "HbA1c_level", "blood_glucose_level"]
     for field in required_fields:
-        if field not in input_data or not input_data[field]:
+        if field not in input_data or input_data[field] == "":
             return False
     return True
 
@@ -17,29 +16,26 @@ def validate_input(input_data):
 def page():
     if request.method == "POST":
         input_data = {
-            "age": float(request.form.get("age")),
-            "hypertension": int(request.form.get("hypertension")),
-            "heart_disease": int(request.form.get("heart_disease")),
-            "bmi": float(request.form.get("bmi")),
-            "HbA1c_level": float(request.form.get("HbA1c_level")),
-            "blood_glucose_level": float(request.form.get("blood_glucose_level"))
+            "age": request.form.get("age"),
+            "hypertension": request.form.get("hypertension"),
+            "heart_disease": request.form.get("heart_disease"),
+            "bmi": request.form.get("bmi"),
+            "HbA1c_level": request.form.get("HbA1c_level"),
+            "blood_glucose_level": request.form.get("blood_glucose_level")
         }
 
         if validate_input(input_data):
             url = "te.csv"
-            data = pd.read_csv(url, header=None)
-            flower = data.values
-
-            # Split
-            x = flower[:, :6]
-            y = flower[:, 6]
+            data = pd.read_csv(url)
+            x = data.iloc[:, :-1]
+            y = data.iloc[:, -1]
 
             model = LogisticRegression()
             model.fit(x, y)
 
             try:
-                arr = model.predict([[input_data["age"], input_data["hypertension"], input_data["heart_disease"],
-                                      input_data["bmi"], input_data["HbA1c_level"], input_data["blood_glucose_level"]]])
+                input_values = [float(input_data[field]) for field in input_data]
+                arr = model.predict([input_values])
                 result = str(arr[0])
             except:
                 result = "Error occurred during prediction"
