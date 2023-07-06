7d7caad98e806fcdb4d9a951c0d2bd00011c4177
Glitch (glitch-hello-website) <none>
2023-07-06 19:10:05 +0000

    🙅👳 Checkpoint ./server.py:59577872/236 ./requirements.txt:59577872/7

--- server.py
+++ server.py
@@ -5,30 +5,30 @@ from sklearn.linear_model import LogisticRegression
 
 app=Flask(__name__)
 
-@app.route('/',methods=["POST"])
+@app.route('/', methods=['POST'])
 def diabetesprediction():
-  if request.method== 'POST' :
-  Age=eval(request.form["n1"])
-  HyperTension=eval(request.form["n2"])
-  HeartDisease=eval(request.form["n3"])
-  Bmi=eval(request.form["n4"])
-  HbA1clevel=eval(request.form["n5"])
-  BloodGlucoseLevel=eval(request.form["n6"])
+  if request.method == 'POST' :
+    age = eval(request.form['n1'])
+    ht = eval(request.form['n2'])
+    hd = eval(request.form['n3'])
+    bmi = eval(request.form['n4'])
+    hbl = eval(request.form['n5'])
+    bgl = eval(request.form['n6'])
   
-  url = "dia.csv"
-  data = pd.read_csv(url,header=None)
-  flower=data.values
+    url = "dia.csv"
+    data = pd.read_csv(url,header=None)
+    flower=data.values
   
   # split the values into input and output
-  x=flower[:,7]
-  y=flower[:,7]
+    x = flower[:,7]
+    y = flower[:,7]
   
-  model = LogisticRegression()
-  model.fit(x,y)
+    model = LogisticRegression()
+    model.fit(x,y)
   
-  arr = model.predict([[Age,HyperTension, HeartDisease,Bmi,HbA1clevel,BloodGlucoseLevel]])
+    arr = model.predict([[age,ht, hd,bmi,hbl,bgl]])
   
-  return render_template("index.html", result=str(arr[0]))
+    return render_template("index.html", result=str(arr[0]))
 
 if __name__ == '__main__':
   app.run(debug=True)
